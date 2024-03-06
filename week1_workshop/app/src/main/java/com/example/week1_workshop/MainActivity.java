62fdcbe81bc77e68d62ccd43cf1abe9935c60f6c
j-talev <j.talev@outlook.com>
2024-03-06 14:41:46 +1100

    add counter feature

--- week1_workshop/app/src/main/java/com/example/week1_workshop/MainActivity.java
+++ week1_workshop/app/src/main/java/com/example/week1_workshop/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.week1_workshop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int count;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button nextBtn = findViewById(R.id.nextBtn);
+        Button clearBtn = findViewById(R.id.clearBtn);
+        textView = findViewById(R.id.textView);
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                incrementCount();
+                updateTextView();
+            }
+        });
+
+        clearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearCount();
+                updateTextView();
+            }
+        });
+    }
+
+    private void incrementCount(){
+        count++;
+        System.out.println("Count: " + count);
+    }
+
+    private void updateTextView() {
+        textView.setText(String.format("Count: %d", count));
+    }
+
+    private void clearCount() {
+        count = 0;
+        System.out.println("Count: " + count);
     }
 }
