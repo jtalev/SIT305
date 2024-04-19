db4a4b40b49d53a6555b6a954a3851fe0a6e243d
j-talev <j.talev@outlook.com>
2024-04-19 18:56:37 +1000

    add ui

---
+++ 5.1c/iTubeApp/app/src/main/java/com/example/itubeapp/PlayActivity.java
@@ -0,0 +1,25 @@
+package com.example.itubeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class PlayActivity extends AppCompatActivity {
+    ImageButton backButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+
+        backButton = findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
